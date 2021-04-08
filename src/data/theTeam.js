2af192c796a821eeed8481b2a8c62f7d8c02dd36
unknown <twessar42@gmail.com>
2021-04-08 11:40:48 -0700

    theTeam data almost complete for team member cards

---
+++ src/data/theTeam.js
@@ -0,0 +1,58 @@
+/** @format */
+
+const theTeam = [
+	{
+		name: 'Tim Mitzen',
+		title: 'Founder + Social Media Mage + Staff Sage',
+		boringTitle: 'CEO + Social Media Manager + Human Resources',
+		myCTA:
+			'I had an idea and I was passionate about it. I am grateful that I was able find a group of people to help me on this journey.',
+		myPassion:
+			'I am a system administrator, who is learning to be a software engineer. My passion for video games is what inspired me to create this company. It is the perfect combination of doing what I love and helping those in need. I am excited to start this new and meaningful journey',
+		imgSrc:
+			'https://github.com/Laarzae-Studios/PMR/blob/16774e0bf5838fade6959dc4250de0d56ca24cfb/public/assets/tim-pixel.png',
+	},
+	{
+		name: 'Michelle Hey',
+		title: 'Clerk Commander + Creative Crafter + Staff Sage',
+		boringTitle: 'COO + Graphic Designer + Human Resources',
+		myCTA:
+			'It is an opportunity for me to put my passions and hobbies to use in a way that helps others. I am happy to contribute to a cause, and to make a difference in such a fun way.',
+		myPassion:
+			'I am a software engineer who loves to dabble in digital illustrations and game art, while pampering my pomeranian and playing piano.',
+		imgSrc:
+			'https://github.com/Laarzae-Studios/PMR/blob/16774e0bf5838fade6959dc4250de0d56ca24cfb/public/assets/michelle-pixel.png',
+	},
+	{
+		name: 'Tawnya Wessar',
+		title: 'Money Mage + Web Wizard + Scribe',
+		boringTitle: 'CFO + Web Developer + Author',
+		myCTA: '',
+		myPassion:
+			'I am a full-time bonus mom and homemaker by day: ninja fullstack web development student by night! What never changes? My passion for supporting people while they become their best self. Never forget: remember your why!',
+		imgSrc:
+			'https://github.com/Laarzae-Studios/PMR/blob/16774e0bf5838fade6959dc4250de0d56ca24cfb/public/assets/tawnya-pixel.png',
+	},
+	{
+		name: 'Kyle Nelson',
+		title: 'Audio Tonic Mage',
+		boringTitle: 'Audio Engineer',
+		myCTA:
+			'The opportunity to work on a project that incorporates both my loves of gaming and music is so exciting! Through Project Mage and beyond I hope to inspire and foster an inclusive and diverse gaming community for all.',
+		myPassion:
+			'I love to make music, play games, code, and to share it all with my friends and family.',
+		imgSrc:
+			'https://github.com/Laarzae-Studios/PMR/blob/16774e0bf5838fade6959dc4250de0d56ca24cfb/public/assets/kyle-pixel.png',
+	},
+	{
+		name: 'Ben Solt',
+		title: 'Merrymaking Mastermind + Creative Crafter',
+		boringTitle: 'Game Designer + Graphic Designer',
+		myCTA:
+			'It has been a dream of mine since college to work in the video game industry/create a video game. Working with this group has helped make that dream a reality.',
+		myPassion:
+			'I have a passion for creating art, animation, coding, and enjoy playing video games. Bringing my imaginations to life.',
+		imgSrc:
+			'https://github.com/Laarzae-Studios/PMR/blob/16774e0bf5838fade6959dc4250de0d56ca24cfb/public/assets/ben-pixel.png',
+	},
+];
