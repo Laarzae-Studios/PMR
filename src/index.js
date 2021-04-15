6541d378bbb024d4719c00e5c8b3646d085c3f63
Randy Egan <31398520+ClydeFrog04@users.noreply.github.com>
2021-04-15 17:51:03 -0600

    Update index.js
    :]

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@
  * Remember your why
  **/
 
-import React                       from "react";
-import { render }                  from "react-dom";
+import React from "react";
+import { render } from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 
 // Import stylesheet and Components
 import "./index.css";
-import App                         from "./App";
+import App from "./App";
 
 render(
     <Router>
